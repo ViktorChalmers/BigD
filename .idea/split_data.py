920f027cac8ec7e1b74865846164b27e4a1df14d
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-03-31 11:27:23 +0200

    FIXED SHITPOOP

---
+++ .idea/split_data.py
@@ -0,0 +1,9 @@
+def split_data(data, n=10):
+    X_train=[]
+    X_test=[]
+    y_train=[]
+    y_test=[]
+    for i in range(n):
+        X_train[i], X_test[i], y_train[i], y_test[i] = train_test_split(X, y, train_size=1/n, test_size=1/n)
+
+    return X_train, X_test, y_train, y_test
--- Vicke_dirt.py
+++ Vicke_dirt.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
+def split_data(X, y, n=10):
+    X_train=[None]*n
+    X_test=[None]*n
+    y_train=[None]*n
+    y_test=[None]*n
+
+    for i in range(n):
+        X_train[i], X_test[i], y_train[i], y_test[i] = train_test_split(X, y, train_size=1/n, test_size=1/n)
+
+    return X_train, X_test, y_train, y_test
+
 # Load UCI breast cancer dataset with column names and remove ID column
+
 uci_bc_data = pd.read_csv(
     "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data",
     sep=",",
@@ -27,12 +39,25 @@ X = uci_bc_data.drop("diagnosis", axis=1).to_numpy()
 ones=y[y==1]
 zeros = y[y==0]
 print(f"%Ones: {len(ones)/len(y)*100},%Zeros: {len(zeros)/len(y)*100}")
-print(X)
 
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 import numpy as np
-clf = QuadraticDiscriminantAnalysis()
-clf.fit(X, y)
+from sklearn.model_selection import train_test_split
+
+'''
+Observation of each class is drawn from a normal distribution (same as LDA).
+QDA assumes that each class has its own covariance matrix (different from LDA).
+'''
+#X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.1, test_size=0.1)
+batch_size = 4
+X_train, X_test, y_train, y_test = split_data(X, y, batch_size)
+print(len(y_train))
+for i in range(batch_size):
+    clf = QuadraticDiscriminantAnalysis()
+    clf.fit(X_train[i], y_train[i])
+
+
+
 #QDA(priors=None, reg_param=0.0)
-print(clf.predict([X[1]]))
+print(clf.predict([X_test[1][1]]))
 print(clf.predict([np.ones(30)]))
