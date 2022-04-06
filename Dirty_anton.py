d200d1ba797401ca69786c809d6ed416a94f04c2
ViktorChalmers <vikwik@student.chalmers.se>
2022-04-06 12:14:28 +0200

    j

--- Dirty_anton.py
+++ Dirty_anton.py
@@ -32,6 +32,20 @@ def dicksnballs(data_size = 1000):
     class2 = np.array(dick1+dick2)
     return [class1, class2]
 
+def plot_decision_boundary(clf, X, Y, cmap='Paired_r'):
+    h = 0.02
+    x_min, x_max = X[:,0].min() - 10*h, X[:,0].max() + 10*h
+    y_min, y_max = X[:,1].min() - 10*h, X[:,1].max() + 10*h
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.figure(figsize=(5,5))
+    plt.contourf(xx, yy, Z, cmap=cmap, alpha=0.25)
+    plt.contour(xx, yy, Z, colors='k', linewidths=0.7)
+    plt.scatter(X[:,0], X[:,1], c=Y, cmap=cmap, edgecolors='k');
+
 data_size=1000
 
 
@@ -86,4 +100,10 @@ plt.plot(class1[:, 0], class1[:, 1], 'o', label="class1")
 plt.plot(class2[:, 0], class2[:, 1], '*', label="class2")
 plt.title('Cross Validation accuracy: QDA: %.3f +/- %.3f ' % (np.mean(scores_QDA), np.std(scores_QDA))+', Cart:  %.3f +/- %.3f' %(np.mean(scores_cart), np.std(scores_cart)))
 plt.legend()
+#plt.figure()
+plot_decision_boundary(clf_cart, X, Y, cmap='Paired_r')
+plt.title('CART accuracy:  %.3f +/- %.3f' %(np.mean(scores_cart), np.std(scores_cart)))
+#plt.figure()
+plot_decision_boundary(clf, X, Y, cmap='Paired_r')
+plt.title('Accuracy: QDA: %.3f +/- %.3f ' % (np.mean(scores_QDA), np.std(scores_QDA)))
 plt.show()
