f274ae10acb3d824bc75081ce542c299f30357d9
Jestmeister <63909272+Jestmeister@users.noreply.github.com>
2022-04-28 14:25:42 +0200

    s

---
+++ Exercise DOS/q1_task4.py
@@ -0,0 +1,72 @@
+from matplotlib.colors import Normalize
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import davies_bouldin_score
+from jqmcvi import base #Dunn index
+
+data = pd.read_csv('C:/Users/Jesper/OneDrive/Dokument/GitHub/BigD/Exercise DOS/data.csv',index_col=0,low_memory=False)
+
+normalized = normalize(data)
+data_scaled = pd.DataFrame(normalized)
+variance = data_scaled.var()
+columns = data.columns
+meanVariance = variance.mean()
+
+variable = []
+
+for i in range(0,len(variance)):
+    if variance[i] >=  meanVariance: #setting the threshold as mean variance
+        variable.append(columns[i])
+
+#print(f"Dropping {len(variance)-len(variable)} features < mean variance")
+
+# creating a new dataframe using the above variables
+new_data = data[variable]
+
+#Normailizing the variance dropped data
+drop_n_norm = normalize(new_data)
+
+
+pca = PCA()
+pca.fit(drop_n_norm)
+
+
+chungus_len = 10 #Found to be good
+pca = PCA(n_components=chungus_len)
+pca.fit(drop_n_norm)
+x_pca=pd.DataFrame(pca.transform(drop_n_norm))
+
+
+#iterate over different cluster sizes and internal clusters
+cluster_range = range(2,7+1)
+silhouette = np.zeros(len(cluster_range))
+davies_b = np.zeros(len(cluster_range))
+
+for j,i in enumerate(cluster_range):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(x_pca) #or new data??
+    silhouette[j] = silhouette_score(x_pca,kmeans.labels_)
+    davies_b[j] = davies_bouldin_score(x_pca,kmeans.labels_)
+
+plt.plot(cluster_range,silhouette)
+plt.title('K-Means')
+plt.xlabel('Cluster Count')
+plt.ylabel('Silhouette Score')
+plt.figure()
+plt.plot(cluster_range,davies_b)
+plt.title('K-Means')
+plt.xlabel('Cluster Count')
+plt.ylabel('Davies Bouldin Score')
+
+plt.show()
+
+
+#2 do: Compare with true label
+#sklearn.metrics.homogeneity_score compare with true data
+
