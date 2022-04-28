95cc383ca6a5a6d7c2fd448cc651ec336c67f851
Jestmeister <63909272+Jestmeister@users.noreply.github.com>
2022-04-28 15:03:13 +0200

    a

--- Exercise DOS/q1_task4.py
+++ Exercise DOS/q1_task4.py
@@ -6,10 +6,39 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import seaborn as sns
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-from sklearn.metrics import davies_bouldin_score
-from jqmcvi import base #Dunn index
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 
+def plot_cluster_ind():
+    #iterate over different cluster sizes and internal clusters
+    cluster_range = range(2,8+1)
+    silhouette = np.zeros(len(cluster_range))
+    davies_b = np.zeros(len(cluster_range))
+    calinski_h = np.zeros(len(cluster_range))
+
+    for j,i in enumerate(cluster_range):
+        kmeans = KMeans(n_clusters=i)
+        kmeans.fit(x_pca) 
+        silhouette[j] = silhouette_score(x_pca,kmeans.labels_)
+        davies_b[j] = davies_bouldin_score(x_pca,kmeans.labels_)
+        calinski_h[j] = calinski_harabasz_score(x_pca,kmeans.labels_)
+
+    plt.plot(cluster_range,silhouette)
+    plt.title('K-Means')
+    plt.xlabel('Cluster Count')
+    plt.ylabel('Silhouette Score')
+    plt.figure()
+    plt.plot(cluster_range,davies_b)
+    plt.title('K-Means')
+    plt.xlabel('Cluster Count')
+    plt.ylabel('Davies-Bouldin Score')
+    plt.figure()
+    plt.plot(cluster_range,calinski_h)
+    plt.title('K-Means')
+    plt.xlabel('Cluster Count')
+    plt.ylabel('Calinski-Harabasz Score')
+    plt.show()
+
+'''
 data = pd.read_csv('C:/Users/Jesper/OneDrive/Dokument/GitHub/BigD/Exercise DOS/data.csv',index_col=0,low_memory=False)
 
 normalized = normalize(data)
@@ -42,31 +71,18 @@ pca = PCA(n_components=chungus_len)
 pca.fit(drop_n_norm)
 x_pca=pd.DataFrame(pca.transform(drop_n_norm))
 
-
-#iterate over different cluster sizes and internal clusters
-cluster_range = range(2,7+1)
-silhouette = np.zeros(len(cluster_range))
-davies_b = np.zeros(len(cluster_range))
-
-for j,i in enumerate(cluster_range):
-    kmeans = KMeans(n_clusters=i)
-    kmeans.fit(x_pca) #or new data??
-    silhouette[j] = silhouette_score(x_pca,kmeans.labels_)
-    davies_b[j] = davies_bouldin_score(x_pca,kmeans.labels_)
-
-plt.plot(cluster_range,silhouette)
-plt.title('K-Means')
-plt.xlabel('Cluster Count')
-plt.ylabel('Silhouette Score')
-plt.figure()
-plt.plot(cluster_range,davies_b)
-plt.title('K-Means')
-plt.xlabel('Cluster Count')
-plt.ylabel('Davies Bouldin Score')
-
-plt.show()
+#plots indiceses for different cluster counts
+#plot_cluster_ind()
 
 
-#2 do: Compare with true label
-#sklearn.metrics.homogeneity_score compare with true data
+
+'''
+#Plot true labels
+a = pd.read_csv('C:/Users/Jesper/OneDrive/Dokument/GitHub/BigD/Exercise DOS/labels.csv',index_col=0)
+print(a)
+print(a.columns)
+#x_pca['labels'] = pd.read_csv('C:/Users/Jesper/OneDrive/Dokument/GitHub/BigD/Exercise DOS/labels.csv',index_col=0)
+
+#sns.pairplot(x_pca ,hue='labels')
+#plt.show()
 
