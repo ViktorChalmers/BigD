bd0da8857b3b4b7abc22a572ab6b474a1028daf3
ViktorChalmers <vikwik@student.chalmers.se>
2022-05-02 15:35:55 +0200

    push

--- Exercise DOS/Viktor/Uppg2.py
+++ Exercise DOS/Viktor/Uppg2.py
@@ -1,4 +1,5 @@
 from matplotlib.colors import Normalize
+from tqdm import tqdm,trange
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import normalize
@@ -62,37 +63,82 @@ x_pca_shuffle = shuffle(x_pca)
 num_folds = 50
 length = int(len(x_pca_shuffle)/num_folds) #length of each fold
 folds = []
+#x_pca_shuffle = np.array(x_pca_shuffle)
 
 for i in range(num_folds-1):
     folds += [x_pca_shuffle[i*length:(i+1)*length]]
 folds += [x_pca_shuffle[num_folds-1*length:len(x_pca_shuffle)]]
 #print(folds)
 k = 5
-print(len(folds))
+#print(len(folds))
+print(folds[0])
 
 model = [KMeans(n_clusters=k).fit(dataset) for dataset in folds]
 
+for i in range(len(folds)):
+    folds[i] = np.array(folds[i])
+#plt.scatter(folds[0][:, 0], folds[0][:, 1], c=model[0].labels_)
+#plt.show()
+
 #print(model[0].labels_)
 
-
 #print(model[0].predict(folds[1]))
-
-M = [np.zeros([801, 801]) for i in range(num_folds-1)]
-J = [np.zeros([801, 801]) for i in range(num_folds-1)]
+print(len(x_pca_shuffle))
+nrDataPoints = len(x_pca_shuffle)
+M = [np.zeros([nrDataPoints, nrDataPoints]) for i in range(num_folds)]
+J = [np.zeros([nrDataPoints, nrDataPoints]) for i in range(num_folds)]
+print(len(M))
+print(M[0].size)
 x_pca_shuffle = np.array(x_pca_shuffle)
+i = 3
+j = 1
+print(x_pca_shuffle[i])
+def findFold(fold, point, point2):
+    ret = False
+    if point in fold and point2 in fold:
+        ret = True
+    return ret
+    #for j in range(len(folds[k])):
+     #   if np.array_equal(folds[k][j], point):
+      #      for i in range(len(folds[k])):
+       #         if np.array_equal(folds[k][i],point2):
+        #            return ret
 
-for k in range(num_folds-1):
+#print(findFold(folds,x_pca_shuffle[10]))
+
+#for k in range(num_folds):
+#    modfit = model[k].predict(x_pca_shuffle)
+#    for i in range(801):
+#        for j in range(801):
+#            if modfit[i] == modfit[j]:
+#                M[k][i,j] = 1
+#            if findFold(folds,x_pca_shuffle[i]) == findFold(folds,x_pca_shuffle[j]):
+#                J[k][i,j] = 1
+#print(J)
+
+
+for k in trange(num_folds):
     modfit = model[k].predict(x_pca_shuffle)
-    for i in range(801):
-        for j in range(801):
+    for i in range(nrDataPoints):
+        for j in range(nrDataPoints):
             if modfit[i] == modfit[j]:
                 M[k][i,j] = 1
-            if np.floor(i/(length-1-1e-3)) == np.floor(j/(length-1-1e-3)):
+            if findFold(folds[k], x_pca_shuffle[i], x_pca_shuffle[j]):
                 J[k][i,j] = 1
+print(sum(M))
+print(J)
+#print(np.sum(J))
+sum = np.zeros([nrDataPoints, nrDataPoints])
 
-pp = pprint.PrettyPrinter()
-pp.pprint(sum(J))
+for i in range(len(J)):
+    sum += J[i]
+print(sum)
 
-print(int(85/80))
+#pp = pprint.PrettyPrinter()
+#pp.pprint(sum(J))
+
+#print(int(85/80))
 #print(np.shape(sum(M)))
-C = sum(M)/sum(J)
+#C = sum(M)/sum(J)
+#print(C)
+
