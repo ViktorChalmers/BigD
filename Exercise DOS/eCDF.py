654e82608ef9189d06940ac7086fa8ebc5ada5ef
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-05-02 14:28:22 +0200

    latest push

--- Exercise DOS/eCDF.py
+++ Exercise DOS/eCDF.py
@@ -1,8 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def eCDF(Mat, q):
     Mat_flatten = Mat.flatten()
     F = 0
     for num in Mat_flatten:
         if num <= q:
             F += 1/len(Mat_flatten)
+    return F
+
+def visualiseCDF(Mat, label='', num_points=np.linspace(0, 1, 100)):
+    Mat_flatten = Mat.flatten()
+    F = np.zeros(len(num_points))
+    for i, q in enumerate(num_points):
+        for num in Mat_flatten:
+            if num <= q:
+                F[i] += 1 / len(Mat_flatten)
+    plt.plot(F, label=label)
+
     return F
 
