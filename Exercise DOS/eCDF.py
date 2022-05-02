3122c4ddb692dea511c8e2d0db07e7a27b263916
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-05-02 14:02:29 +0200

    latest push

--- Exercise DOS/eCDF.py
+++ Exercise DOS/eCDF.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def eCDF(Mat, q):
     Mat_flatten = Mat.flatten()
     F = 0
@@ -7,6 +5,4 @@ def eCDF(Mat, q):
         if num <= q:
             F += 1/len(Mat_flatten)
     return F
-C = np.ones([10, 10])
 
-print(eCDF(C,1))
