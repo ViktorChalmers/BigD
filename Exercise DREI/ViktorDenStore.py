61aa3d47c8b3ef62aa91eaf9cd882a4eee5c8495
vwiklund <v.wiklund@hotmail.com>
2022-05-12 16:15:44 +0200

    s

--- Exercise DREI/ViktorDenStore.py
+++ Exercise DREI/ViktorDenStore.py
@@ -1,5 +1,7 @@
 import tarfile
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, chi2, f_classif
 
 try:
     data = pd.read_pickle("data.pkl")
@@ -27,3 +29,22 @@ X = data.loc[:, data.columns != target]
 Y = data.loc[:, data.columns == target]
 print(X.shape)
 print(Y.shape)
+print(labels)
+print(Y)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=8)
+
+
+#print(X.columns[0])
+select_feature = SelectKBest(k=200).fit(x_train, y_train) # dont want entire data for overfitting reasons
+
+selected_features_df = pd.DataFrame({'Feature':list(x_train.columns),
+                                     'Scores':select_feature.scores_})
+print(selected_features_df.sort_values(by='Scores', ascending=False))
+
+x_train_chi = select_feature.transform(x_train)
+print(x_train_chi)
+
+
+
+
