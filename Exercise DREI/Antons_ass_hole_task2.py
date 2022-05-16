bbd763c9c28a5d7724e9079856bdcbd150cb6e19
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-05-16 17:48:54 +0200

    latest push

--- Exercise DREI/Antons_ass_hole_task2.py
+++ Exercise DREI/Antons_ass_hole_task2.py
@@ -21,8 +21,12 @@ label = pd.read_csv("TCGA-PANCAN-HiSeq-801x20531/labels.csv", index_col=0)
 
 selectK = SelectKBest(score_func=f_classif, k=200)
 new_data = selectK.fit_transform(data, label)
-
-x_train, x_test, y_train, y_test = train_test_split(new_data, label, test_size=0.2, random_state=8)
+'''
+samples=label[label['Class'] == 'PRAD']
+data_class = new_data[]
+print(samples)
+'''
+#x_train, x_test, y_train, y_test = train_test_split(new_data, label, test_size=0.2, random_state=8)
 
 C_list = np.logspace(-4, 4, 30)
 #TODO split data into validation and training, implement LogisticRegressionCV
@@ -39,22 +43,33 @@ print(f'score = {score}\nf1 score = {score_f1}')
 
 C = C_list[6]
 '''
-feature_select_list = np.zeros(200)
+feature_select_list = np.zeros([5, 200])
 
 for i in trange(50):
-    x_train, x_test, y_train, y_test = train_test_split(new_data, label, test_size=0.2, random_state=8, shuffle=True)
+    x_train, x_test, y_train, y_test = train_test_split(new_data, label, test_size=0.05, random_state=8)
+
     #clf = LogisticRegression(multi_class='ovr', solver='liblinear', intercept_scaling=10000, C=C, penalty='l1')
     clf = LogisticRegressionCV(multi_class='ovr', solver='liblinear', intercept_scaling=10000, Cs=C_list, penalty='l1',
                                cv=5)
     clf.fit(x_train, y_train)
+    for j in range(5):
+        feature_importance = abs(clf.coef_[j])
+        top_5_ching = [x[0] for x in enumerate(feature_importance) if x[1] > 0]
+        print(f'Index for top 5 features for class {j} = {top_5_ching}')
+        for index in top_5_ching:
+            feature_select_list[j][index] += 1
 
-    feature_importance = abs(clf.coef_[0])
-    top_5_ching = [x[0] for x in sorted(enumerate(feature_importance), key=lambda x: x[1])[-5:]]
-    print(f'Index for top 5 features = {top_5_ching}')
-    for index in top_5_ching:
-        feature_select_list[index] += 1
 
-plt.bar(np.linspace(1, 200, 200), feature_select_list*2, width=2)
+def plot_viktors_dick(feature_list):
+    plt.figure()
+    plt.bar(np.linspace(1, 200, 200), feature_list, width=0.5)
+    top_5_largest = [x[0] for x in sorted(enumerate(feature_list), key=lambda x: x[1])[-5:]]
+    plt.xticks(top_5_largest, top_5_largest)
+
+
+for k in range(5):
+    plot_viktors_dick(feature_select_list[k])
+
 plt.ylabel('Precentage %')
 plt.xlabel('Feature #')
 plt.show()
