35fb4ef2fceb9c28acf27f97f7fa57339938816a
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-05-16 12:53:26 +0200

    latest push

--- Exercise DREI/Antons_ass_hole_task2.py
+++ Exercise DREI/Antons_ass_hole_task2.py
@@ -2,6 +2,7 @@ import tarfile
 import pandas as pd
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,12 +19,13 @@ label = pd.read_csv("TCGA-PANCAN-HiSeq-801x20531/labels.csv", index_col=0)
 
 selectK = SelectKBest(score_func=f_classif, k=200)
 new_data = selectK.fit_transform(data, label)
-print(new_data)
+
+x_train, x_test, y_train, y_test = train_test_split(new_data, label, test_size=0.5, random_state=8)
 
 C_list = np.logspace(-4, 4, 30)
 #TODO split data into validation and training, implement LogisticRegressionCV
-clf = LogisticRegression(multi_class='ovr', solver='liblinear', intercept_scaling=10000, C=C_list[10])
-clf.fit(new_data, label)
+clf = LogisticRegression(multi_class='ovr', solver='liblinear', intercept_scaling=10000, C=C_list[10], penalty='l1')
+clf.fit(x_train, y_train)
 
 feature_importance = abs(clf.coef_[0])
 top_5_ching = [x[0] for x in sorted(enumerate(feature_importance), key=lambda x: x[1])[-5:]]
@@ -31,7 +33,11 @@ print(f'Index for top 5 features = {top_5_ching}')
 feature_select_list = np.zeros(200)
 for index in top_5_ching:
     feature_select_list[index] += 1
-
-plt.bar(np.linspace(1,200, 200), feature_select_list, width=5)
+prediction=clf.predict(x_test)
+print(y_test)
+print(prediction)
+score = clf.score(x_test, y_test)
+print(f'score = {score}')
+plt.bar(np.linspace(1, 200, 200), feature_select_list, width=5)
 plt.show()
 
