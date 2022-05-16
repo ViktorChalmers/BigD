0134c21f3d360b2fd157628425e0bde75650aba7
ViktorChalmers <vikwik@student.chalmers.se>
2022-05-16 19:33:44 +0200

    Push it to the limits

--- Exercise DREI/ViktorDenStore.py
+++ Exercise DREI/ViktorDenStore.py
@@ -2,6 +2,9 @@ import tarfile
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, chi2, f_classif
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 try:
     data = pd.read_pickle("data.pkl")
@@ -22,6 +25,8 @@ print(f"data is loaded as {type(data)} with shape: {data.shape}")
 
 data["Class"] = labels
 target = "Class"
+
+print(labels[labels["Class"] == "PRAD"])
 print(data.head())
 print(data.Class.value_counts())
 
@@ -41,10 +46,39 @@ select_feature = SelectKBest(k=200).fit(x_train, y_train) # dont want entire dat
 selected_features_df = pd.DataFrame({'Feature':list(x_train.columns),
                                      'Scores':select_feature.scores_})
 print(selected_features_df.sort_values(by='Scores', ascending=False))
-
-x_train_chi = select_feature.transform(x_train)
-print(x_train_chi)
-
-
+#print(selected_features_df)
+scores = np.array(selected_features_df.sort_values(by='Scores', ascending=False))
+print(scores[:,1])
 
 
+r = 200
+plo = scores[0:r]
+print(plo)
+#plt.hist(scores[0:200, 1],bins=200)
+plt.bar(np.linspace(0,r,r),plo[:,1])
+#plt.show()
+
+#sns.barplot(x="day", y="total_bill", data=plo[:,1], capsize=.1, ci="sd")
+#sns.swarmplot(x="day", y="total_bill", data=plo[:,1], color="0", alpha=.35)
+
+
+plt.title("Top 200 features")
+plt.xlabel("Features sorted by f-score")
+plt.ylabel("Score function")
+plt.show()
+#plt.plot(selected_features_df.sort_values(by='Scores', ascending=False))
+#plt.show()
+
+#def histedges_equalN(x, nbin):
+ #   npt = len(x)
+ #   return np.interp(np.linspace(0, npt, nbin + 1),
+ #                    np.arange(npt),
+#                     np.sort(x))
+
+#x = np.random.randn(100)
+#n, bins, patches = plt.hist(x, histedges_equalN(x, 10))
+#plt.show()
+
+
+#x_train_chi = select_feature.transform(x_train)
+#print(x_train_chi)
