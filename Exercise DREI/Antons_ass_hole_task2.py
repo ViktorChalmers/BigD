7a9ff4cd858c4f68964e2edebe300f0f69260fee
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-05-17 15:40:28 +0200

    latest push

--- Exercise DREI/Antons_ass_hole_task2.py
+++ Exercise DREI/Antons_ass_hole_task2.py
@@ -10,6 +10,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
 
+
+def get_C1(clf, num_folds):
+    C1=np.zeros(5)
+    for i, score in enumerate(clf.scores_):
+        cv_mean = np.mean(clf.scores_[score], axis=0)
+        cv_std = np.std(clf.scores_[score], axis=0)
+        idx_max_mean = np.argmax(cv_mean)
+        idx_C1 = np.where(
+            (cv_mean <= cv_mean[idx_max_mean] + cv_std[idx_max_mean] / np.sqrt(num_folds)) &
+            (cv_mean >= cv_mean[idx_max_mean])
+        )[0][0]
+        C1[i] = clf.Cs_[idx_C1]
+    return C1
+
+
 try:
     data = pd.read_pickle("data.pkl")
 except:
@@ -44,15 +59,26 @@ print(f'score = {score}\nf1 score = {score_f1}')
 
 C = C_list[6]
 '''
+
+'''
+x_train, y_train = resample(new_data, label, n_samples=round(0.95*np.shape(new_data)[0]))
+clf = LogisticRegressionCV(multi_class='ovr', solver='liblinear', intercept_scaling=10000, Cs=C_list, penalty='l1',
+                               cv=5)
+clf.fit(x_train, y_train)
+C = clf.C_
+'''
 feature_select_list = np.zeros([5, 200])
-print(np.shape(new_data))
+num_folds = 5
 for i in trange(50):
     #x_train, x_test, y_train, y_test = train_test_split(new_data, label, test_size=0.05, random_state=8)
-    x_train, y_train = resample(new_data, label, n_samples=round(0.95*np.shape(new_data)[0]))
+    x_train, y_train = resample(new_data, label, n_samples=round(0.95*np.shape(new_data)[1]))
     #clf = LogisticRegression(multi_class='ovr', solver='liblinear', intercept_scaling=10000, C=C, penalty='l1')
     clf = LogisticRegressionCV(multi_class='ovr', solver='liblinear', intercept_scaling=10000, Cs=C_list, penalty='l1',
-                               cv=5)
+                               cv=num_folds, scoring='f1')
     clf.fit(x_train, y_train)
+
+    C1 = get_C1(clf, num_folds)
+    print(f'C = {clf.C_}, C1 = {C1}')
     for j in range(5):
         feature_importance = abs(clf.coef_[j])
         top_5_ching = [x[0] for x in enumerate(feature_importance) if x[1] > 0]
@@ -61,17 +87,18 @@ for i in trange(50):
             feature_select_list[j][index] += 1
 
 
-def plot_viktors_dick(feature_list):
+def plot_viktors_dick(feature_list, num):
     plt.figure()
     plt.bar(np.linspace(1, 200, 200), feature_list, width=0.5)
     top_5_largest = [x[0] for x in sorted(enumerate(feature_list), key=lambda x: x[1])[-5:]]
     plt.xticks(top_5_largest, top_5_largest)
-
+    plt.savefig('class ' + str(num))
 
 for k in range(5):
-    plot_viktors_dick(feature_select_list[k])
+    plot_viktors_dick(feature_select_list[k], k)
 
-plt.ylabel('Precentage %')
-plt.xlabel('Feature #')
+#plt.ylabel('Precentage %')
+#plt.xlabel('Feature #')
 plt.show()
+
 
