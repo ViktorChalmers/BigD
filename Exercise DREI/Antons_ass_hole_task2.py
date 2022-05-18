a06f51382a7b9b377426556190078982a13ab7dd
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2022-05-18 17:48:33 +0200

    latest push

--- Exercise DREI/Antons_ass_hole_task2.py
+++ Exercise DREI/Antons_ass_hole_task2.py
@@ -69,6 +69,7 @@ C = clf.C_
 '''
 feature_select_list = np.zeros([5, 200])
 num_folds = 5
+coeff = np.zeros([5, 200])
 for i in trange(100):
     x_train, y_train = resample(new_data, label, n_samples=round(0.95*np.shape(new_data)[0]))
     #clf = LogisticRegression(multi_class='ovr', solver='liblinear', intercept_scaling=10000, C=C, penalty='l1')
@@ -80,26 +81,44 @@ for i in trange(100):
     print(f'C = {clf.C_}, C1 = {C1}')
 
     for j in range(5):
+        coeff[j] += abs(clf.coef_[j])/100
         feature_importance = abs(clf.coef_[j])
-        top_5_ching = [x[0] for x in enumerate(feature_importance) if x[1] > 0]
-        print(f'Index for top 5 features for class {j} = {top_5_ching}')
-        for index in top_5_ching:
+        top_ching = [x[0] for x in enumerate(feature_importance) if x[1] > 0]
+        print(f'Index for top 5 features for class {j} = {top_ching}')
+        for index in top_ching:
             feature_select_list[j][index] += 1
 
 
-def plot_viktors_dick(feature_list, num):
+def plot_histo(feature_list, num):
     classes = clf.classes_
     plt.figure()
     plt.bar(np.linspace(1, 200, 200), feature_list, width=0.5)
-    top_5_largest = [x[0] for x in sorted(enumerate(feature_list), key=lambda x: x[1])[-5:]]
-    plt.savefig('class ' + str(num))
+    if len([x[0] for x in sorted(enumerate(feature_list)) if x[1]==100])>5:
+        top_5_largest=[x[0] for x in sorted(enumerate(feature_list)) if x[1]==100]
+
+        while len(top_5_largest) > 5:
+            coeff_list = [coeff[num][i] for i in top_5_largest]
+            top_5_largest.pop(coeff_list.index(min(coeff_list)))
+    else:
+        top_5_largest = [x[0] for x in sorted(enumerate(feature_list), key=lambda x: x[1])[-5:]]
+
     plt.title(classes[num]+'\nTop 5 features: '+str(top_5_largest))
     plt.xlabel('Feature')
     plt.ylabel('Count')
+    plt.savefig('class ' + str(num))
 
+
+def checkList(count_list):
+    element = count_list[0]
+    check = True
+    for item in count_list:
+        if element != item:
+            check = False
+            return check
+    return check
 
 for k in range(5):
-    plot_viktors_dick(feature_select_list[k], k)
+    plot_histo(feature_select_list[k], k)
 
 plt.show()
 
